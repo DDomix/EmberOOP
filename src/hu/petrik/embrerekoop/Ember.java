02988bb48083ec3e7383f5eee9bd74e36446e8c6
DDomix <papp.dominik@petrik.hu>
2022-09-20 14:28:57 +0200

    Ember osztaly -Születési év,hónap,nap meghatározása -Életkor meghatározása csak év alapján(LocalDate)

--- src/hu/petrik/embrerekoop/Ember.java
+++ src/hu/petrik/embrerekoop/Ember.java
@@ -1,5 +1,7 @@
 package hu.petrik.embrerekoop;
 
+import java.time.LocalDate;
+
 public class Ember {
     private String nev;
     private String SzulDatum;
@@ -11,8 +13,22 @@ public class Ember {
         SzulHely = szulHely;
     }
 
+    public int getSzuletesiEv(){
+        return Integer.parseInt(SzulDatum.substring(0, 4));
+    }
+    public int getSzuletesiHonap(){
+        return Integer.parseInt(SzulDatum.split("-")[1]);
+    }
+    public int getSzuletesiNap(){
+        return Integer.parseInt(SzulDatum.split("-")[2]);
+    }
+    public int getEletkor(){
+        LocalDate maidatum = LocalDate.now();
+        return maidatum.getYear()-this.getSzuletesiEv();
+    }
+
     @Override
     public String toString() {
-        return String.format("%-30s %10s %20s",this.nev,this.SzulDatum,this.SzulHely);
+        return String.format("%-30s %10s (%d) %20s",this.nev,this.SzulDatum,this.getEletkor(), this.SzulHely);
     }
 }
